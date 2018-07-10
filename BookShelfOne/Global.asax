bc4d97b1ce67302022dcd9bec38de74f65f5d246
edanielbluford <e.daniel.bluford@gmail.com>
2018-07-10 18:22:41 -0400

    first commit

---
+++ BookShelfOne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookShelfOne.MvcApplication" Language="C#" %>
---
+++ BookShelfOne/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookShelfOne.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DisplayName("Date Published")]
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
